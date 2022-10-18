cb70475b7f9817b521c50ec828b7dae9c403b3b1
jwi <jan.wiesbauer@smarter-ecommerce.com>
2022-10-18 15:31:14 +0200

    Push orders ot rabbitmq for processing

--- src/main/kotlin/io/geanik/kotlin_mongo_rabbit_tutorial/backend/logic/OrderService.kt
+++ src/main/kotlin/io/geanik/kotlin_mongo_rabbit_tutorial/backend/logic/OrderService.kt
@@ -1,7 +1,9 @@
 package io.geanik.kotlin_mongo_rabbit_tutorial.backend.logic
 
+import com.rabbitmq.client.ConnectionFactory
 import io.geanik.kotlin_mongo_rabbit_tutorial.backend.model.Order
 import org.springframework.stereotype.Service
+import java.nio.charset.StandardCharsets
 
 interface OrderService {
 
@@ -10,7 +12,12 @@ interface OrderService {
 }
 
 @Service
-class OrderServiceImpl(private val customerService: CustomerService) : OrderService {
+class OrderServiceImpl(
+    private val customerService: CustomerService,
+    private val orderProcessingDelegator: OrderProcessingDelegator
+) : OrderService {
+
+    private val factory = ConnectionFactory()
 
     override fun insertOrder(order: Order, customerId: Int): Order {
         val customer = customerService.findCustomerById(customerId)
@@ -20,6 +27,7 @@ class OrderServiceImpl(private val customerService: CustomerService) : OrderServ
             customerService.updateCustomer(customer)
         } ?: throw IllegalArgumentException("Customer not found")
 
+        orderProcessingDelegator.delegateProcessing(order)
         return order
     }
 
